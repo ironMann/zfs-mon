2961c54c34454585c8d8aab00aea6d0a8418b5f4
Gvozden Neskovic <neskovic@gmail.com>
2014-06-27 15:05:59 +0200

    debian package for ganglia monitoring

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
 # Regenerate all necessary files...
-aclocal &&
-autoconf &&
-autoheader &&
-automake
-if [ $? -ne 0 ]; then
-echo "Error: Failed to regenerate Makefiles and configure scripts."
-fi
+autoreconf -fiv
+rm -Rf autom4te.cache
+
